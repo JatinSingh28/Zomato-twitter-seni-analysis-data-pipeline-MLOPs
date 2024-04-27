2cf8863b8c8bbae9f2060e121bd3314e33ef2579
Jatin Singh <sagoisinghjatin9951@gmail.com>
2024-04-27 02:30:31 +0000

    first commit

---
+++ twitter_dags/config.py
@@ -0,0 +1,12 @@
+config = {
+    "aws_key": "<your_aws_key>",
+    "aws_secret": "<your_aws_secret>",
+    "twitter_auth_token": "<your_twitter_auth_token>",
+    "redshift": {
+        "dbname": "<your_redshift_dbname>",
+        "user": "<your_redshift_user>",
+        "password": "<your_redshift_password>",
+        "host": "<your_redshift_host>",
+        "port": "<your_redshift_port>",
+    },
+}
